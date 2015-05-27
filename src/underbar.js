815a17e7ecc434a180b818b1e714139ff12c0ffd
Nick Balestra <nick@balestra.ch>
2015-05-27 10:02:52 +0200

    every and some implemented and passes all the tests

--- src/underbar.js
+++ src/underbar.js
@@ -205,12 +205,26 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(wasTrue, value){
+      if (wasTrue) {
+        if (iterator) {
+          return iterator(value) ? true : false;
+        } else {
+          return (value) ? true : false;
+        }
+      } else {
+        return false;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return !_.every(collection, function(value){
+      return (iterator) ? !iterator(value) : !value;
+    });
   };
 
 
