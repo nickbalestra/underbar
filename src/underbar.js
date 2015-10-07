486d9b1872eca3edd84ef9e51d5572f91a1b98c9
Nick Balestra <nick@balestra.ch>
2015-10-06 18:49:36 -0700

    Cleaned up collections part

--- src/underbar.js
+++ src/underbar.js
@@ -1,11 +1,11 @@
-(function() {
+(() => {
   'use strict';
 
   window._ = {};
 
-  // Returns whatever value is passed as the argument. This function doesn't
-  // seem very useful, but remember it--if a function needs to provide an
-  // iterator when the user does not pass one in, this will be handy.
+  // Returns whatever value is passed as the argument. 
+  // If a function needs to provide an iterator when the user 
+  // does not pass one in, use the identity function.
   _.identity = function(val) {
     return val;
   };
@@ -14,30 +14,20 @@
    * COLLECTIONS
    * ===========
    *
-   * In this section, we'll have a look at functions that operate on collections
-   * of values; in JavaScript, a 'collection' is something that can contain a
+   * Functions that operate on collections of values; 
+   * in JavaScript, a 'collection' is something that can contain a
    * number of values--either an array or an object.
-   *
-   *
-   * IMPORTANT NOTE!
-   * ===========
-   *
-   * The .first function is implemented for you, to help guide you toward success
-   * in your work on the following functions. Whenever you see a portion of the
-   * assignment pre-completed, be sure to read and understanding it fully before
-   * you proceed. Skipping this step will lead to considerably more difficulty
-   * implementing the sections you are responsible for.
    */
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
-  _.first = function(array, n) {
+  _.first = (array, n) => {
     return n === undefined ? array[0] : array.slice(0, n);
   };
 
-  // Like first, but for the last elements. If n is undefined, return just the
-  // last element.
-  _.last = function(array, n) {
+  // Like first, but for the last elements. 
+  // If n is undefined, return just the last element.
+  _.last = (array, n) => {
     if (n === undefined) {
       return array[array.length - 1];
     } else {
@@ -47,10 +37,7 @@
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
-  //
-  // Note: _.each does not have a return value, but rather simply runs the
-  // iterator function over each item in the input collection.
-  _.each = function(collection, iterator) {
+  _.each = (collection, iterator) => {
     if (Array.isArray(collection)) {
       for(var i = 0; i < collection.length; i++) {
         iterator(collection[i], i, collection);
@@ -65,9 +52,6 @@
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
-    // TIP: Here's an example of a function that needs to iterate, which we've
-    // implemented for you. Instead of using a standard `for` loop, though,
-    // it uses the iteration helper `each`, which you will need to write.
     var result = -1;
 
     _.each(array, function(item, index) {
@@ -94,8 +78,6 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
     return _.filter(collection, function(item){
       return !(test(item));
     });
@@ -126,9 +108,6 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
     var results = [];
 
     _.each(collection, function(item){
@@ -138,19 +117,11 @@
     return results;
   };
 
-  /*
-   * TIP: map is really handy when you want to transform an array of
-   * values into a new array of values. _.pluck() is solved for you
-   * as an example of this.
-   */
-
+  
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
-    // TIP: map is really handy when you want to transform an array of
-    // values into a new array of values. _.pluck() is solved for you
-    // as an example of this.
     return _.map(collection, function(item){
       return item[key];
     });
@@ -178,11 +149,11 @@
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
 
-    _.each(collection, function(item){
+    _.each(collection, function(item, index, collection){
       if (accumulator === undefined) {
         accumulator = item;
       } else {
-        accumulator = iterator(accumulator, item)
+        accumulator = iterator(accumulator, item, index, collection)
       };
     });
 
@@ -191,8 +162,6 @@
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function(collection, target) {
-    // TIP: Many iteration problems can be most easily expressed in
-    // terms of reduce(). Here's a freebie to demonstrate!
     return _.reduce(collection, function(wasFound, item) {
       if (wasFound) {
         return true;
@@ -204,7 +173,6 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
     return _.reduce(collection, function(wasTrue, value){
       if (wasTrue) {
         if (iterator) {
@@ -221,7 +189,6 @@
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
     return !_.every(collection, function(value){
       return (iterator) ? !iterator(value) : !value;
     });
